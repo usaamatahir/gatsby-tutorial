e70d6e445f6f0482601d14d3fed023c58bc01eb2
usaamatahir <usamatahir8330@gmail.com>
2020-11-07 14:38:51 +0500

    DOT_ENV

--- gatsby_step_12_dotenv/gatsby-config.js
+++ gatsby_step_12_dotenv/gatsby-config.js
@@ -1,5 +1,18 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
 module.exports = {
     plugins: [
-
+        'gatsby-plugin-typescript',
+        {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENT_FUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENT_FUL_ACCESS_TOKEN,
+      },
+    },
     ]
 }
